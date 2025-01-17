96ce23033940e52c9bbd2b12e0c27d4c6b6a0611
Agbai C Joshua <realacjoshu@gmail.com>
2025-01-17 13:55:58 +0100

    Still debugging the Home not rendering issue (AnimatedSection Check)

--- frontend/src/components/AnimatedSection.tsx
+++ frontend/src/components/AnimatedSection.tsx
@@ -8,16 +8,9 @@ interface AnimatedSectionProps {
 
 const AnimatedSection = ({ children, className = '' }: AnimatedSectionProps) => {
   return (
-    <motion.div
-      initial={{ opacity: 0, y: 20 }}
-      whileInView={{ opacity: 1, y: 0 }}
-      viewport={{ once: true, margin: "-100px" }}
-      transition={{ duration: 0.6 }}
-      className={className}
-    >
+    <div className={className}>
       {children}
-    </motion.div>
+    </div>
   );
 };
-
 export default AnimatedSection;
